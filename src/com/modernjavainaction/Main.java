ae9ae423ae98971d44d7f5f3801d674133ca7047
underdarks <rkdrl45617@gmail.com>
2022-06-27 08:22:32 +0900

    init

---
+++ src/com/modernjavainaction/Main.java
@@ -0,0 +1,9 @@
+package com.modernjavainaction;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("args = " + args);
+    }
+}
